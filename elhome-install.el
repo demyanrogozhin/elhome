47b7f11cc6b530373c7deffe984fb1d67000248f
Dave Abrahams <dave@saturn.local>
2011-02-18 23:13:36 -0500

    Install all the parts with el-get
    Change file extension on README

--- elhome-install.el
+++ elhome-install.el
@@ -1,13 +1,33 @@
-(let ((el-get-sources 
-       '(el-get
-         (:name elhome
-                  :type git
-                  :url "git://github.com/dabrahams/elhome.git"
-                  :features elhome))))
-
-  (unless (require 'el-get nil t)
+(progn
+  (setq debug-on-error t)
+  (defun elhome-setup () 
+    (let ((el-get-sources 
+           '(el-get
+             (:name initsplit
+                    :type git
+                    :url "git://github.com/dabrahams/initsplit.git"
+                    :features initsplit)
+             byte-code-cache
+             markdown-mode
+             (:name elhome
+                    :type git
+                    :url "git://github.com/dabrahams/elhome.git"
+                    :features elhome))))
+      (el-get 'sync)
+      (find-file-read-only
+       (concat
+        (file-name-as-directory user-emacs-directory) 
+        "el-get/elhome/README.mkd"))
+      (markdown-mode)
+      (message "Thank you for installing elhome")
+      ))
+  
+  (add-to-list 'load-path (concat
+                           (file-name-as-directory user-emacs-directory) 
+                           "el-get/el-get"))
+  (if (require 'el-get nil t)
+      (elhome-setup)
     (url-retrieve
-     "https://github.com/dabrahams/el-get/raw/master/el-get-install.el"
-     (lambda (s) (end-of-buffer) (eval-print-last-sexp))))
-
-  (el-get))
+     "https://gist.github.com/raw/834740/9bd42b0f9508ed4dd278b3ff82059688660582d6/el-get-install.el"
+     (lambda (s) (end-of-buffer) (eval-print-last-sexp) 
+       (elhome-setup)))))
