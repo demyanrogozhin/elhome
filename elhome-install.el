db2d4e4500eb204aeaed88ed94cb1fa757f8eb2f
Dave Abrahams <dave@saturn.local>
2011-02-20 07:39:38 -0500

    Remove asynchrony; improve user experience

--- elhome-install.el
+++ elhome-install.el
@@ -1,60 +1,52 @@
-(progn
+(let ((el-get-sources 
+       '((:name initsplit :compile nil)
+         byte-code-cache
+         markdown-mode
+         elhome))
+      (debug-on-error t))
+
   (defun el-get-subdir (&rest d)
     (mapconcat 'file-name-as-directory
                (append (list user-emacs-directory "el-get") d) ""))
 
-  (defun elhome-setup () 
-    (message "Switching to dabrahams' el-get...")
+  (unless (require 'el-get nil t)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://github.com/dimitri/el-get/raw/master/el-get-install.el")
+      (end-of-buffer)
+      (eval-print-last-sexp)))
 
-    ;; warning: do *NOT* try to factor this out, even though it looks
-    ;; like code repetition
-    (let* ((default-directory (el-get-subdir "el-get"))
-           (git (executable-find "git")))
+  (switch-to-buffer "*Messages*")
+  (message "*\n*\n* Patience please; ELHOME is installing...\n* \n*")
 
-      (unless (zerop (call-process git nil nil t 
-                                   "remote" "set-url" "origin" 
-                                   "https://github.com/dabrahams/el-get.git"))
-        (error "unable to point origin at dabrahams' el-get"))
-      (unless (zerop (call-process git nil nil t "fetch" "origin"))
-        (error "unable to fetch from dabrahams/el-get"))
-      (unless (zerop (call-process git nil nil t "checkout" "origin/master"))
-        (error "unable to checkout dabrahams' master"))
+  (message "Switching to dabrahams' el-get...")
 
-      (load-library "el-get")
+  (let* ((default-directory (el-get-subdir "el-get"))
+         (git (executable-find "git")))
 
-      (message "Grabbing elhome dependencies...")
-      (let ((el-get-sources 
-             '((:name el-get :compile nil)
-               (:name initsplit :compile nil)
-               byte-code-cache
-               markdown-mode
-               elhome)))
+    (unless (zerop (call-process git nil nil t 
+                                 "remote" "set-url" "origin" 
+                                 "https://github.com/dabrahams/el-get.git"))
+      (error "unable to point origin at dabrahams' el-get"))
+    (unless (zerop (call-process git nil nil t "fetch" "origin"))
+      (error "unable to fetch from dabrahams/el-get"))
+    (unless (zerop (call-process git nil nil t "checkout" "origin/master"))
+      (error "unable to checkout dabrahams' master")))
 
-        (save-window-excursion
-          (el-get 'sync))
-
-        (require 'elhome)
-
-        (with-current-buffer
-            (find-file-noselect
-             (concat (el-get-subdir "elhome") "README.markdown"))
-          (goto-char (point-max))
-          (search-backward "## Congratulations")
-          (switch-to-buffer (current-buffer))
-          (recenter 'top))
-        (message "Thank you for installing elhome!")
-        )))
-  
   (add-to-list 'load-path (el-get-subdir "el-get"))
 
-  (if (require 'el-get nil t)
-      (elhome-setup)
-    (url-retrieve
-     "https://github.com/dimitri/el-get/raw/master/el-get-install.el"
-     (lambda (s) 
-       (end-of-buffer) 
+  ;; Reload el-get after switching to dwamacs
+  (load-library "el-get")
 
-       (let ((el-get-sources 
-              '((:name el-get :compile nil))))
-         (eval-print-last-sexp)
-         (elhome-setup))))))
+  (message "Grabbing elhome dependencies...")
+  (el-get 'sync)
+
+  (with-current-buffer
+      (find-file-noselect
+       (concat (el-get-subdir "elhome") "README.markdown"))
+    (goto-char (point-max))
+    (search-backward "## Congratulations")
+    (switch-to-buffer (current-buffer))
+    (recenter 'top))
+
+  (message "Thank you for installing elhome!"))
