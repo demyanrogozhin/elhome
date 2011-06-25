995b1124106854c7f84a6983affe63198046fe7f
Dave Abrahams <dave@boostpro.com>
2011-06-24 17:46:50 -0700

    Install asynchronously; keep the *Messages* buffer visible while doing it

--- elhome-install.el
+++ elhome-install.el
@@ -1,5 +1,8 @@
 (let ((debug-on-error t))
 
+  (switch-to-buffer "*Messages*")
+  (message "*\n*\n* Patience please; ELHOME is installing...\n* \n*")
+
   (setq el-get-byte-compile nil)
   (unless (require 'el-get nil t)
     (with-current-buffer
@@ -9,19 +12,23 @@
       (eval-print-last-sexp)))
 
   (switch-to-buffer "*Messages*")
-  (message "*\n*\n* Patience please; ELHOME is installing...\n* \n*")
+
   (add-hook 'el-get-post-install-hooks 'el-get-init)
 
-  (let ((el-get-sources
-         '((:name elhome :depends (initsplit byte-code-cache)))))
-    (el-get 'wait '(elhome)))
+  (setq el-get-sources
+         '((:name elhome
+            :depends (initsplit byte-code-cache))))
 
-  (with-current-buffer
-      (find-file-noselect
-       (concat (el-get-package-directory "elhome") "README.markdown"))
-    (goto-char (point-max))
-    (search-backward "## Congratulations")
-    (switch-to-buffer (current-buffer))
-    (recenter 'top))
+  (eval-after-load 'elhome
+    '(with-current-buffer
+        (find-file-noselect
+         (concat (el-get-package-directory "elhome") "README.markdown"))
+       (elhome-init)
+       (goto-char (point-max))
+       (search-backward "## Congratulations")
+       (switch-to-buffer (current-buffer))
+       (recenter 'top)))
+
+  (el-get nil '(elhome))
+
   (message "Thank you for installing elhome!"))
-
